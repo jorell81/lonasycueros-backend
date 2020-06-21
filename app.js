f5d876273dce79214230f3fe90e5d923d35bed17
William Marin <williammarinalvarado@gmail.com>
2020-06-21 17:41:28 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 
 
 // Inicializar variables
 var app = express();
+
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Importa rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 
 
@@ -18,12 +31,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, resp) 
 });
 
 // Rutas
-app.get('/', (req, resp, next) => {
-    resp.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 
